2c4b4d5b705fb45eaeb6cf0d0f8c51b485b052be
Cecilia Daninthe <cecilia.daninthe@gmail.com>
2021-12-20 15:20:50 -0500

    commit tree d07f96ff6215a50188daf39a5df3cec79559bd2d

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup) {
+    const headquarters = 42;
+    return Math.abs(pickup - headquarters);
+}
+
+function distanceFromHqInFeet(pickup){
+    return (distanceFromHqInBlocks(pickup))*264;
+}
+
+function distanceTravelledInFeet(pickup, destination){
+    return Math.abs((pickup-destination)*264);
+}
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+    let fare;
+    if (distance <= 400){
+        fare = 0;
+    }
+    else if ((distance > 400) && (distance < 2000)){
+        fare = (distance - 400) * 0.02;
+    }
+    else if ((distance > 2000) && (distance < 2500)){
+        fare = 25;
+    }
+    else if (distance > 2500){
+        return 'cannot travel that far';
+    }
+    return fare;
+  }
